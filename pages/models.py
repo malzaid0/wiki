48c36b0718882b85e014195a7f8158e6f953bec2
Mohammed Alzaid <m.al-zaid@live.com>
2020-08-30 15:53:14 +0300

    completed wiki task

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Page(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return f"http://127.0.0.1:8000/page/detail/{self.id}"
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Page
 
-# Create your views here.
+def List(request):
+    context = {
+        "pages": Page.objects.all()
+    }
+    return render(request, "list.html", context)
+
+
+def Detail(request, page_id):
+    context = {
+        "page": Page.objects.get(id=page_id)
+    }
+    return render(request, "detail.html", context)
